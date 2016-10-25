3f350370dcd6f2e335a812118ba12c8a732a2310
Jannis Leidel <jannis@leidel.info>
2016-10-25 09:00:50 +0200

    Because Windows

--- envdir/test_envdir.py
+++ envdir/test_envdir.py
@@ -246,7 +246,10 @@ def test_keyboard_interrupt(run, tmpenvdir, monkeypatch):
     with py.test.raises(Response) as response:
         run('envdir', str(tmpenvdir), 'sleep', '1')
     if platform.system() == 'Windows':
-        assert response.value.status == signal.SIGINT
+        if py.std.sys.version_info[:2] == (2, 6):
+            assert response.value == signal.SIGINT
+        else:
+            assert response.value.code == signal.SIGINT
     else:
         # Minus sign is added by subprocess to distinguish signals from exit
         # codes. Since we send a signal within the test to stop the process,
